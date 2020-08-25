f6691a3b026c699beffea6229c38bd18d2213315
Vlad <VladimirGerusUA@gmail.com>
2020-08-25 21:08:21 +1000

    added : initial project layout, web UI, js VM, jquery dependency

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeoTrack.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetValues(string request)
+        {
+            var res = new ResultObj[] { new ResultObj { EngineName = request, Entries = new int[] {1,2,3 } }, new ResultObj { EngineName = "e2", Entries = new int[] { 4, 5, 6 } } };
+            return Ok(res);
+        }
+    }
+
+    public class ResultObj
+    {
+        public string EngineName { get; set; }
+        public IEnumerable<int> Entries { get; set; }
+    }
+
+}
---
+++ wwwroot/Scripts/temp.js
@@ -0,0 +1,16 @@
+﻿
+
+function onSearchClick()
+{
+    $.get("Home/GetValues", { request: 'ferty' },
+        (res) =>
+        {
+            let container = $('#result');
+            res.forEach(item =>
+            {
+                let line = item.engineName + ' ' + item.entries;
+                container.append(line);
+                container.append('<br/>');
+            });
+        }, "json");
+}
