22b6276092589143af5d35b9e9894224f05a4b8e
Vlad <VladimirGerusUA@gmail.com>
2020-08-27 00:50:48 +1000

    added : unit tests, README

---
+++ README.md
@@ -0,0 +1,35 @@
+# SeoTrack
+
+1. Loads mock pages of two search engines (google and bing)
+2. Parses those pages to find what positions in the search results is held by https://www.infotrack.com.au
+3. Presents results on a web page
+
+# building and running
+## from Visual Studio
+1. Right click solution and select Restore nuget packages for solution
+2. Right click solution and select Restore client side libraries
+3. Rebuild solution
+4. Run SeoTrack project
+
+## from command line
+1. from the root folder of the repository run "dotnet restore"
+2. from the SeoTrack folder run "libman restore" (if libman is not istalled first run "dotnet tool install -g Microsoft.Web.LibraryManager.Cli")
+3. from the SeoTrack folder run "dotnet run"
+4. to run tests from the root of the repository run "dotnet test"
+
+## from docker WIP
+
+
+
+# Points of interest
+
+## class structure
+Arguably excessive for this task built as a model for more complicated project. Optimized towards extesibility, dependency injection and mockability/testability.
+
+## tchnologies/Libraries
+1. Jquery for ajax call
+2. libman to keep jquery (and potentially aother libs in the future) from git repository to keep it light
+3. dependency injection
+4. ASP.net core to be able to wrap build/exection in a lightweight linux docker container
+5. NUnit, moq to make sure that classes meet requirements that were set
+
---
+++ SeoTrackTests/SearchEngineParserTests.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using NUnit.Framework;
+using SeoTrack.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeoTrackTests
+{
+    //TODO: Add test for page address calculation
+    //TODO: Add tests for page parsers
+    class SearchEngineParserTests
+    {
+        static readonly SearchEngineParameters DefaultParameters = new SearchEngineParameters { SearchEngineName = "Name", UrlTemplate = "{0}" };
+
+        [Test]
+        public async Task TestNoLinks() // making sure GetPositions() returns 0 when no results found
+        {
+            var urlFinder = new Mock<ISearchPageParser>();
+            urlFinder
+                .Setup(finder => finder.GetLinks(It.IsAny<string>()))
+                .Returns(new string[] { "some site" });
+
+            var engineParser = new SearchEngineParser(DefaultParameters, Mock.Of<IPageLoader>(), urlFinder.Object);
+
+            var positions = await engineParser.GetPositions("", "non-existing site", 100);
+
+            Assert.That(positions.Count() == 1);
+            Assert.That(positions.First() == 0);
+        }
+
+        [Test]
+        public async Task TestSearchLimit() // making sure parsers stops after search limit is reached
+        {
+            string siteUrl = "https://somesite.com";
+            int searchLimit = 50;
+
+            var urlFinder = new Mock<ISearchPageParser>();
+            urlFinder
+                .Setup(finder => finder.GetLinks(It.IsAny<string>()))
+                .Returns(new string[] { siteUrl, siteUrl, siteUrl });
+
+            var engineParser = new SearchEngineParser(DefaultParameters, Mock.Of<IPageLoader>(), urlFinder.Object);
+
+            var positions = await engineParser.GetPositions("", siteUrl, searchLimit);
+
+            Assert.That(positions.Count() == searchLimit);
+        }
+
+        [Test]
+        public void TestSearchEngineName() // making sure parsers respects given name
+        {
+            var engineParser = new SearchEngineParser(DefaultParameters, Mock.Of<IPageLoader>(), Mock.Of<ISearchPageParser>());
+
+            Assert.That(engineParser.SearchEngineName == DefaultParameters.SearchEngineName);
+        }
+    }
+}
